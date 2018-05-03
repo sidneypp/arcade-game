591104ac63334cc274a41a92430b3cc78b736d2d
Sidney Oliveira de Souza <oliveira.sidney96@gmail.com>
2018-05-02 22:09:54 -0300

    feat: The Player function with player sprite and player initial location

--- js/app.js
+++ js/app.js
@@ -25,10 +25,26 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = 200;
+    this.y= 400;
+}
+
+Player.prototype.update = function(dt) {
+    
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
+let player = new Player();
 
 
 
